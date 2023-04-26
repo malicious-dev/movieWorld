4d09e64d5651113eba44d71cff5db9095b18bee4
malicious-dev <Programming1hars@gmail.com>
2023-04-26 07:37:10 +0530

    fix date

--- src/pages/home/upcoming/UpComing.jsx
+++ src/pages/home/upcoming/UpComing.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Carousel from "../../../components/carousel/Carousel";
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import Carousel from '../../../components/carousel/Carousel';
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
 
-import useFetch from "../../../hooks/useFetch";
+import useFetch from '../../../hooks/useFetch';
 
 const Popular = () => {
-  const [endpoint, setEndpoint] = useState("movie");
+  const [endpoint, setEndpoint] = useState('movie');
 
   const { data, loading } = useFetch(`/${endpoint}/upcoming`);
-
+  console.log(data);
   // const onTabChange = (tab) => {
   //   setEndpoint(tab === "Movies" ? "movie" : "tv");
   // };
@@ -24,11 +24,7 @@ const Popular = () => {
           onTabChange={ onTabChange }
         /> */}
       </ContentWrapper>
-      <Carousel
-        data={ data?.results }
-        loading={ loading }
-        endpoint={ endpoint }
-      />
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
     </div>
   );
 };
