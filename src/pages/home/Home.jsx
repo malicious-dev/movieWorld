49bc2eb6e5c32863282d9b8731b9d67f6d749aeb
malicious-dev <Programming1hars@gmail.com>
2023-04-26 05:36:49 +0530

    added upcmoing and now playing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,11 +6,15 @@ import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
 import TopRated from "./topRated/TopRated";
+import UpComing from "./upcoming/UpComing";
+import Latest from "./latest/Latest";
 
 const Home = () => {
     return (
         <div className="homePage">
             <HeroBanner />
+            <UpComing />
+            <Latest />
             <Trending />
             <Popular />
             <TopRated />
